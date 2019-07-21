36c4c1fed1c4c1c0516fbe15cd99ae51b5a14b2b
M. Fairuzi Teguh <mfairuzit@gmail.com>
2019-07-22 08:47:17 +0900

    Add logo

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,9 +3,15 @@
     <h1 class="text-center">
       Jadwal SIAK War
     </h1>
-    <router-link :to="{ name: 'create-schedule' }">
-      Create Schedule
-    </router-link>
+    <div class="text-center row justify-content-center">
+      <div class="col-xl-4 col-lg-6 col-md-8 col-sm-10">
+        <img
+          class="img-fluid mb-4"
+          src="@/assets/logo.png"
+        >
+        <h5><i>Susun jadwalmu dan jadilah pemenangnya!</i></h5>
+      </div>
+    </div>
   </div>
 </template>
 
